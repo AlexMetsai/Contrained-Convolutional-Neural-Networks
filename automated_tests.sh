c449467f21811f0d0fe1c7db268c8fde55943981
Alexandros Metsai <alexmetsai@gmail.com>
2019-06-04 10:17:19 +0300

    Update automated_tests.sh

--- automated_tests.sh
+++ automated_tests.sh
@@ -5,3 +5,9 @@
 # MIT License
 
 # Test the network for all the trained instances.
+
+# Define the folder containing the weights
+FOLDER="saved_model/"
+
+# An already existing folder, where results will be moved to.
+LOG_FOLDER="logs/"
