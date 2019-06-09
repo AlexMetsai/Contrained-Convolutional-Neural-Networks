6850a750067dbb1bfd0821de89709ebc6ee3d1d8
Alexandros Metsai <alexmetsai@gmail.com>
2019-06-09 14:59:33 +0300

    added notice

--- test_videos.py
+++ test_videos.py
@@ -1,1 +1,10 @@
+'''
+Alexandros I. Metsai
+alexmetsai@gmail.com
+MIT License
 
+Test the accuracy of the contrained convolutional neural 
+network based on predictions of video frame images.
+A threshold value is used to make the 
+decision for the overall classification.
+'''
