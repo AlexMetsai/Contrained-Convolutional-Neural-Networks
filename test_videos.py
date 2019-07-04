8f4cd55b209df8f394fab5c143d06093ca2288d3
Alexandros Metsai <alexmetsai@gmail.com>
2019-07-04 10:01:44 +0300

    Update test_videos.py

--- test_videos.py
+++ test_videos.py
@@ -86,6 +86,9 @@ for i in range(len(video_folders)):
     
     print(prediction_acc)
 
+correct_guesses_neg = correct_guesses
+negative_len = len(video_folders)
+
 # ****************************
 # *** Test positive videos ***
 # ****************************
